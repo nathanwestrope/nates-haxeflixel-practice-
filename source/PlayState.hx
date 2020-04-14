94c5dc6614cc43b405bdec2fecaa1c97f9b8cdf8
nate_is_west <lobsterboi@gmail.com>
2020-04-13 20:07:17 -0500

    added "hello from the other side" to playstate

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,14 @@
 package;
 
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var text = new FlxText(275, 250, 100, "Hello from the other side!");
+		add(text);
 		super.create();
 	}
 
