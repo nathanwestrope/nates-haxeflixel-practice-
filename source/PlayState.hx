0a2c4fd1cccbabe04cc86186d85774057f1ec2e4
nate_is_west <lobsterboi@gmail.com>
2020-04-20 22:50:31 -0500

    edits to Hero and playstate

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,18 +1,45 @@
 package;
 
+import haxe.display.Protocol.HaxeResponseErrorData;
 import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	override public function create()
+	override public function create():Void
 	{
-		var text = new FlxText(200, 225, 0, "Hello from the other side!", 20, true);
-		add(text);
 		super.create();
+		var text = new FlxText(0, 0, 0, "Hello from the other side!", 25, true);
+		text.screenCenter();
+		this.color
+		add(text);
+
+		for (i in 0...15)
+		{
+			var spr = new Hero(100, 100);
+			add(spr);
+		}
+
+		for (i in 0...15)
+		{
+			var spr = new Hero(0, 0);
+			add(spr);
+		}
+		
+		for (i in 0...15)
+		{
+			var spr = new Hero(450, 350);
+			add(spr);
+		}
+
+		for (i in 0...15)
+		{
+			var spr = new Hero(500, 550);
+			add(spr);
+		}
 	}
 
-	override public function update(elapsed:Float)
+	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 	}
